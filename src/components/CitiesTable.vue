d9c716a483513f6838023183506ec26ba4b0da72
MariosOiko <m.economopoulos1@gmail.com>
2024-06-11 12:34:20 +0300

    using yarn

--- src/components/CitiesTable.vue
+++ src/components/CitiesTable.vue
@@ -29,25 +29,25 @@ export default {
     },
   },
 };
+
+// .cities-table {
+//   width: 100%;
+//   border-collapse: collapse;
+//   margin-top: 100px;
+//   margin-left: 250px;
+// }
+
+// .cities-table th,
+// .cities-table td {
+//   border: 1px solid #ddd;
+//   padding: 8px;
+//   text-align: left;
+// }
+
+// .cities-table th {
+//   background-color: #f2f2f2;
+//   font-weight: bold;
+// }
 </script>
 
-<style scoped lang="scss">
-.cities-table {
-  width: 100%;
-  border-collapse: collapse;
-  margin-top: 100px;
-  margin-left: 250px;
-}
-
-.cities-table th,
-.cities-table td {
-  border: 1px solid #ddd;
-  padding: 8px;
-  text-align: left;
-}
-
-.cities-table th {
-  background-color: #f2f2f2;
-  font-weight: bold;
-}
-</style>
+<style scoped lang="scss"></style>
